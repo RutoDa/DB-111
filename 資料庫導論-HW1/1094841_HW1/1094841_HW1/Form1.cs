56ab427d48d2778be70f0d471518318773d3fa22
UULi <72511520+RutoDa@users.noreply.github.com>
2022-06-05 12:06:51 +0800

    Add files via upload

---
+++ 資料庫導論-HW1/1094841_HW1/1094841_HW1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _1094841_HW1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cn = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                "AttachDbFilename=|DataDirectory|Database1.mdf;" +
+                "Integrated Security=True";
+            SqlConnection db = new SqlConnection(cn);
+            SqlDataAdapter ap = new SqlDataAdapter("Select * From Employee", db);
+            DataSet ds = new DataSet();
+            ap.Fill(ds, "Employee");
+            dataGridView1.DataSource = ds.Tables["Employee"];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cn = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                "AttachDbFilename=|DataDirectory|Database1.mdf;" +
+                "Integrated Security=True";
+                SqlConnection db = new SqlConnection(cn);
+                db.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = db;
+                cmd.CommandText = "Insert Into Employee(Id,Name,Title,Salary) " +
+                    "Values(@id, @name, @title, @salary)";
+                cmd.Parameters.AddWithValue("@id", textBox1.Text);
+                cmd.Parameters.AddWithValue("@name", textBox2.Text);
+                cmd.Parameters.AddWithValue("@title", textBox3.Text);
+                cmd.Parameters.AddWithValue("@salary", textBox4.Text);
+                cmd.ExecuteNonQuery();
+                db.Close();
+                Form1_Load(sender, e);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cn = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                "AttachDbFilename=|DataDirectory|Database1.mdf;" +
+                "Integrated Security=True";
+                SqlConnection db = new SqlConnection(cn);
+                db.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = db;
+                cmd.CommandText = "Update Employee Set Name=@name, Title=@title, Salary=@salary Where Id=@id";
+                cmd.Parameters.AddWithValue("@id", textBox1.Text);
+                cmd.Parameters.AddWithValue("@name", textBox2.Text);
+                cmd.Parameters.AddWithValue("@title", textBox3.Text);
+                cmd.Parameters.AddWithValue("@salary", textBox4.Text);
+                cmd.ExecuteNonQuery();
+                db.Close();
+                Form1_Load(sender, e);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cn = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                "AttachDbFilename=|DataDirectory|Database1.mdf;" +
+                "Integrated Security=True";
+                SqlConnection db = new SqlConnection(cn);
+                db.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = db;
+                cmd.CommandText = "Delete From Employee Where Id=@id";
+                cmd.Parameters.AddWithValue("@id", textBox1.Text);
+                cmd.ExecuteNonQuery();
+                db.Close();
+                Form1_Load(sender, e);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
